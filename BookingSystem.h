c90f93ee786afab31be573c33b42014519245402
Md Alhaz Mondal Hredhay <amhredhay102@gmail.com>
2025-05-08 21:53:57 +0600

    BookingSystem header file has been added

---
+++ BookingSystem.h
@@ -0,0 +1,24 @@
+/*
+    BookingSystem.h
+    ----------------
+    Static utility class for managing movies and bookings.
+*/
+
+#ifndef BOOKINGSYSTEM_H
+#define BOOKINGSYSTEM_H
+#include <string>
+using namespace std;
+
+class BookingSystem
+{
+    public:
+        static void viewMovies();
+        static void addMovie();
+        static void deleteMovie();
+        static void bookTicket(const string& username);
+        static void cancelTicket(const string& username);
+        static void viewBookingsByUser(const string& username);
+        static void viewAllBookings();
+};
+
+#endif
